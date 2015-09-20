ba874de6bd0d4e1d60fe8dbc9033a833cc105dce
Elias Kuiter <info@elias-kuiter.de>
2015-09-20 11:46:56 +0200

    Added two-column layout

--- server/publications.js
+++ server/publications.js
@@ -9,7 +9,7 @@ Meteor.publish("categories", function() {
 Meteor.publish("projectsInCategory", function(categorySlug) {
   check(categorySlug, String);
   return Projects.find({ categorySlug: categorySlug }, {
-    fields: { title: 1, slug: 1, categorySlug: 1, order: 1, imageUrl: 1 }
+    fields: { title: 1, slug: 1, categorySlug: 1, order: 1, images: 1 }
   });
 });
 
